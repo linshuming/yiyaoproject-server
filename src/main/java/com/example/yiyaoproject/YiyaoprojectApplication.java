4ccbcf33011745cd186946659bf3325e315387c4
linshuming <linshuming@matech.cn>
2021-03-08 17:34:43 +0800

    创建项目

---
+++ src/main/java/com/example/yiyaoproject/YiyaoprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.yiyaoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YiyaoprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YiyaoprojectApplication.class, args);
+    }
+
+}
